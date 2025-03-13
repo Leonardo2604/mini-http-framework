89127f4c32fc69d54ad0372b33f10bb16484fea0
Leonardo Rangel dos Santos <leonardorangel2604@gmail.com>
2025-03-13 12:40:24 -0300

    feat: adicionando tratativa para o novo tipo de erro

--- src/http/config/error-handler.ts
+++ src/http/config/error-handler.ts
@@ -2,6 +2,7 @@ import { ErrorHandlerFunction } from '@/lib/http/error-handler-function';
 import { AppError } from '@/modules/shared/errors/app.error';
 import { BusinessError } from '@/modules/shared/errors/business.error';
 import { inProduction } from '@/config/app';
+import { NotFoundError } from '@/modules/shared/errors/not-found.error';
 
 export const errorHandler: ErrorHandlerFunction = async (_, response, error) => {
   if (!inProduction) {
@@ -11,10 +12,13 @@ export const errorHandler: ErrorHandlerFunction = async (_, response, error) => 
   if (error instanceof AppError) {
     if (error instanceof BusinessError) {
       response.status(400).json({ message: error.message }).send();
+      return;
     }
 
-    response.status(500).json({ message: 'Internal Server Error' }).send();
-    return;
+    if (error instanceof NotFoundError) {
+      response.status(404).json({ message: error.message }).send();
+      return;
+    }
   }
 
   response.status(500).json({ message: 'Internal Server Error' }).send();
